c69ff30c244d6070d024a10886dc04006bc9c8a8
Cristian Cerbusca <cristian.cerbusca@gmail.com>
2020-11-27 00:08:25 +0200

    done lr0 functions

--- Formal Languages and Compiler Design/Lab5/Lab5.py
+++ Formal Languages and Compiler Design/Lab5/Lab5.py
@@ -1,5 +1,76 @@
 import json
 
+
+class RHS:
+
+    def __init__(self, s=None, l=None, dot_pos = -1):
+        self._l = l
+        if s is not None:
+            self._l = tuple(s.split(' '))
+        
+        self._dotpos = dot_pos
+
+
+    @property
+    def item_after_dot(self):
+        if self._dotpos != -1 and self._dotpos != len(self._l):
+            return self._l[self._dotpos]
+        return None
+
+    @property
+    def has_dot(self):
+        return self._dotpos != -1
+    
+
+
+    def increment_dot_pos(self):
+        return RHS(l = self.items, dot_pos=self.dot + 1)
+    
+    
+
+    
+    @property
+    def items(self):
+        return self._l
+    
+    @property
+    def dot(self):
+        return self._dotpos
+    
+    @dot.setter
+    def dot(self, value):
+        self._dotpos = value
+
+    def __str__(self):
+        l = self._l
+        if self.has_dot:
+            l = list(l)
+            l.insert(self.dot, '.')
+            l = tuple(l)
+        return str(l)
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def __key(self):
+        return (self._l, self._dotpos)
+    
+    def __hash__(self):
+        return hash(self.__key())
+    
+    def __eq__(self, other):
+        if isinstance(other, RHS):
+            return self.__key() == other.__key()
+        return False
+    
+    def __iter__(self):
+        for it in self._l:
+            yield it
+    
+
+
+
+
 class Grammar:
 
     def __init__(self, file_path):
@@ -9,15 +80,21 @@ class Grammar:
             self._terminals = self._data["terminals"]
             self._start_symbol = self._data["start_symbol"]
             self._productions = self._data["productions"]
+            self._productions['S_'] = [self._start_symbol]
+            self._non_terminals.append('S_')
 
             if self._start_symbol not in self._non_terminals:
                 raise Exception("Start symbol not in non terminals")
+
+            self._productions = {
+                k : tuple(map(lambda x: RHS(s=x), v)) for k, v in self._productions.items()
+            }
 
             for k, v in self._productions.items():
                 if k not in self._non_terminals:
                     raise Exception("Production left hand side symbol not in non terminals")
                 for it in v:
-                    for s in it.split(' '):
+                    for s in it:
                         if s not in self._non_terminals and s not in self._terminals:
                             raise Exception("Production right hand side symbol not in non terminals")
         
@@ -69,12 +146,65 @@ class Grammar:
             else:
                 print("Invalid key")
 
+    def __stringify_prod(self, production):
+        return "{}->{}".format(production[0], ' '.join(production[1]))
+
+    def closure(self, productions):
+        c = list(productions)
+
+        i = 0
+        while i < len(c):
+            p = c[i][1]
+            if p.has_dot:
+                if p.item_after_dot in self._non_terminals:
+                    for item in self._productions[p.item_after_dot]:
+                        it = (p.item_after_dot, RHS(l=item.items, dot_pos=0))
+                        if it not in c:
+                            c.append(it)
+            else:
+                raise Exception("Closure production should contain dot")
+            i += 1
+        return c
+    
+    def goto(self, s, token):
+        c = []
+        for closure_item in s:
+            if closure_item[1].has_dot:
+                if closure_item[1].item_after_dot == token:
+                    c.append((closure_item[0], closure_item[1].increment_dot_pos()))
+            else:
+                raise Exception("Must have dot")
+        return self.closure(c) if len(c) > 0 else None
+    
+    def col_can(self):
+        c = []
+        s0 = self.closure([("S_", RHS(s="S", dot_pos=0))])
+        c.append(s0)
+        i = 0
+        while i < len(c):
+            s = c[i]
+            ws = self._non_terminals + self._terminals
+            for w in ws:
+                g = self.goto(s, w)
+                if g is not None and g not in c:
+                    c.append(g)
+            i += 1
+        return c
+
 
 
 
 if __name__ == "__main__":
     grammar = Grammar("g1.txt")
-    grammar.menu()
+
+    print(
+        '\n'.join(map(str, grammar.col_can()))
+    )
+
+
+    
+
+
         
         
             
